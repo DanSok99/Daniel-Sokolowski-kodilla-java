76409b2f78a8600870a678cde7a6ed8f4988c108
Daniel Sokolowski <sokolowskidaniel99@gmail.com>
2023-05-01 15:45:16 +0200

    First Commit

---
+++ src/main/java/com/example/koddillacourse/FirstClass.java
@@ -0,0 +1,4 @@
+package com.example.koddillacourse;
+
+public class FirstClass {
+}
---
+++ src/test/java/com/example/koddillacourse/KoddillaCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.koddillacourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoddillaCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
