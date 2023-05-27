1e69dafae5c6cd1c84aae8b60ba3f6ebb1dfc8a3
Daniel Sokolowski <sokolowskidaniel99@gmail.com>
2023-05-27 17:27:23 +0200

    8.4

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FindFlight.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FindFlight.java
@@ -1,2 +1,23 @@
-package com.kodilla.exception.test;public class FindFlight {
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindFlight {
+    Map<String, Boolean> airport = new HashMap<>();
+    public FindFlight(Map<String, Boolean> airport){
+        this.airport = airport;
+    }
+    public void findFlight(Flight flight) throws RouteNotFoundException{
+        if(airport.get(flight.getDepartureAirport())){
+            System.out.println("Departure airport is available");
+        }else{
+            throw new RouteNotFoundException();
+        }
+        if(airport.get(flight.getArrivalAirport())){
+            System.out.println("Arrival airport is available");
+        }else{
+            throw new RouteNotFoundException();
+        }
+    }
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/RouteNotFoundExceptionMain.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/RouteNotFoundExceptionMain.java
@@ -1,2 +1,23 @@
-package com.kodilla.exception.test;public class RouteNotFoundExceptionMain {
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RouteNotFoundExceptionMain {
+    public static void main(String[] args) {
+        Map<String, Boolean> airports = new HashMap<>();
+        airports.put("Warszawa", false);
+        airports.put("Poznan", true);
+        airports.put("Krakow", true);
+        airports.put("Lublin", true);
+        airports.put("Gniezno", false);
+
+        FindFlight findFlight = new FindFlight(airports);
+        Flight flight = new Flight("Poznan","Gniezno");
+        try{
+            findFlight.findFlight(flight);
+        } catch (RouteNotFoundException e) {
+            System.out.println("Sorry you can't take a fly, departure or arrival airport is not available");
+        }
+    }
 }
